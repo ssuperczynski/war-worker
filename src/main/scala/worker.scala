f6cd64a48f8feed32a7d1cee1941c81e35a534b4
ssuperczynski <s.superczynski@gmail.com>
2015-06-13 17:46:58 +0200

    queue worker first relase

--- src/main/scala/worker.scala
+++ src/main/scala/worker.scala
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import com.redis._
 
 object worker extends App {
@@ -6,29 +9,68 @@ object worker extends App {
     println("Worker start...")
     val r = new RedisClient("localhost", 6379)
     var i: Int = 0
-//    while (true) {
+
+    while (true) {
       try {
-
-        val list = r.keys("*soldier*")
-
-        list match {
-          case Some(s) => for (k <- s) {
-            val amount = r.hget(k.get, "amount")
-            val added_time = r.hget(k.get, "time")
-            println(amount.get)
-            println(added_time.get)
-          }
-        }
-
-        i = i + 1
         Thread.sleep(1000)
+        println("loop: " + i)
+        i = i + 1
+        val numbers: List[Int] = getUsers(r)
+        checkQueue(r, numbers)
 
       } catch {
         case e: Exception =>
           println(e.getMessage)
       }
 
-//    }
+    }
 
+  }
+
+  private def checkQueue(r: RedisClient, numbers: List[Int]): Unit = {
+    val d1 = new Date()
+    val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val now = new Date()
+    for (nr <- numbers) {
+      val ranges = r.hkeys("user_" + nr + ":soldier:queue_time")
+      ranges match {
+        case Some(ss) => for (range <- ss) {
+          val added_time = r.hget("user_" + nr + ":soldier:queue_time", range)
+          val saved = format.parse(added_time.get)
+          val diff = (now.getTime - saved.getTime) / 1000 // diff in sec
+          val interval = r.hget("user_" + nr + ":soldier:interval", range)
+          val modulo = diff % interval.get.toInt
+          if (modulo == 0) {
+            r.hincrby("user_" + nr + ":soldier:amount", range, 1)
+            val queue_amount = r.hget("user_" + nr + ":soldier:queue_amount", range)
+            if (queue_amount.get.toInt <= 0) {
+              r.hdel("user_" + nr + ":soldier:queue_amount", range)
+              r.hdel("user_" + nr + ":soldier:queue_time", range)
+            } else {
+              r.hincrby("user_" + nr + ":soldier:queue_amount", range, -1)
+            }
+
+          }
+
+        }
+      }
+    }
+    val d2 = new Date()
+    println("diff: -> " + (d2.getTime - d1.getTime))
+  }
+
+  private def getUsers(r: RedisClient): List[Int] = {
+    val list = r.keys("*user_*")
+    var numbers = List[Int]()
+
+    list match {
+      case Some(s) => for (k <- s) {
+        val key = k.get
+        val nr = key.replaceAll("[^0-9?!\\.]", "")
+        numbers = nr.toInt :: numbers
+      }
+      case None =>
+    }
+    numbers
   }
 }
